e4ea9e0a9f6ee4dc602359871103f0f3d6030661
cstodor <todorcs@gmail.com>
2017-02-14 04:56:45 +1200

    Created PageService
    - Imported Http, Observable, rxjs map, and tooltip intercafe
    - added getTooltips() - to get all tooltips
    - added getTooltip() - to get a single tooltip by ID

---
+++ src/app/pages/pages.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+
+import { ITooltip } from '../../api/tooltip';
+
+@Injectable()
+export class PagesService {
+
+    private tooltipsUrl = "api/tooltips.json";
+
+    constructor(private _http: Http) { }
+
+    getTooltips(): Observable<ITooltip[]> {
+        return this._http.get(this.tooltipsUrl)
+            .map(response => <ITooltip[]>response.json().tooltipsData);
+    }
+
+    getTooltip(id: number): Observable<ITooltip> {
+        return this.getTooltips()
+        .map(tooltips => tooltips.find(tooltip => tooltip.id === id));
+    }
+
+}
